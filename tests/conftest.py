215e76516ee19af6b8bfe29fc4b38f1f2dae568a
Lennart Grahl <lennart.grahl@gmail.com>
2016-05-14 23:21:19 +0200

    Add tests for an invalid path value and an invalid client-hello message Add primitive Client class to conftest Add client_factory fixture Add pack_message and unpack_message fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import socket
 import asyncio
 import os
-import functools
 import ssl
+import struct
 
 import pytest
 import websockets
@@ -21,7 +21,8 @@ def pytest_namespace():
             os.path.join(os.path.abspath(__file__), os.pardir, 'cert.pem')),
         'subprotocols': [
             saltyrtc.SubProtocol.saltyrtc_v1_0.value
-        ]
+        ],
+        'timeout': 0.01,
     }}
 
 
@@ -50,6 +51,16 @@ def key_path(key_pair):
         - `key_pair`: A :class:`libnacl.public.SecretKey` instance.
     """
     return key_pair.hex_pk().decode()
+
+
+@asyncio.coroutine
+def _sleep(timeout=None):
+    """
+    Sleep *timeout* seconds.
+    """
+    if timeout is None:
+        timeout = pytest.saltyrtc.timeout
+    yield from asyncio.sleep(timeout)
 
 
 @pytest.fixture(scope='module')
@@ -95,6 +106,14 @@ def client_key():
 
 
 @pytest.fixture(scope='module')
+def sleep():
+    """
+    Sleep *timeout* seconds.
+    """
+    return _sleep
+
+
+@pytest.fixture(scope='module')
 def server(request, event_loop, port):
     """
     Return a :class:`saltyrtc.Server` instance.
@@ -114,6 +133,28 @@ def server(request, event_loop, port):
     request.addfinalizer(fin)
 
 
+class Client:
+    def __init__(self, ws_client, pack_message, unpack_message, key=None, timeout=0.1):
+        self.ws_client = ws_client
+        self.pack_and_send = pack_message
+        self.recv_and_unpack = unpack_message
+        self.permanent_key = key
+        self.session_key = None
+        self.timeout = timeout
+
+    def send(self, receiver, message):
+        yield from self.pack_and_send(
+            self.ws_client, receiver, message,
+            key=self.permanent_key, timeout=self.timeout
+        )
+
+    def recv(self):
+        return (yield from self.recv_and_unpack(
+            self.ws_client,
+            key=self.permanent_key, timeout=self.timeout
+        ))
+
+
 @pytest.fixture(scope='module')
 def ws_client_factory(client_key, url, event_loop, server):
     """
@@ -126,22 +167,66 @@ def ws_client_factory(client_key, url, event_loop, server):
     ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     ssl_context.load_verify_locations(cafile=pytest.saltyrtc.cert)
 
-    # Return partial
-    return functools.partial(
-        websockets.connect,
-        '{}/{}'.format(url, key_path(client_key)),
-        subprotocols=pytest.saltyrtc.subprotocols,
-        ssl=ssl_context,
-        loop=event_loop,
-    )
+    def _ws_client_factory(path=None, **kwargs):
+        if path is None:
+            path = '{}/{}'.format(url, key_path(client_key))
+        _kwargs = {
+            'subprotocols': pytest.saltyrtc.subprotocols,
+            'ssl': ssl_context,
+            'loop': event_loop,
+        }
+        _kwargs.update(kwargs)
+        return websockets.connect(path, **_kwargs)
+    return _ws_client_factory
 
 
 @pytest.fixture(scope='module')
-def get_unencrypted_packet(event_loop):
+def client_factory(client_key, url, event_loop, server, pack_message, unpack_message):
+    """
+    Return a simplified :class:`websockets.client.connect` wrapper
+    where no parameters are required.
+    """
+    # Note: The `server` argument is only required to fire up the server.
+
+    # Create SSL context
+    ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ssl_context.load_verify_locations(cafile=pytest.saltyrtc.cert)
+
     @asyncio.coroutine
-    def _get_unencrypted_packet(client, timeout=0.1):
+    def _client_factory(path=client_key, key=client_key, timeout=0.1, **kwargs):
+        _kwargs = {
+            'subprotocols': pytest.saltyrtc.subprotocols,
+            'ssl': ssl_context,
+            'loop': event_loop,
+        }
+        _kwargs.update(kwargs)
+        ws_client = yield from websockets.connect(
+            '{}/{}'.format(url, key_path(path)),
+            **_kwargs
+        )
+        return Client(
+            ws_client, pack_message, unpack_message,
+            key=key, timeout=timeout
+        )
+    return _client_factory
+
+
+@pytest.fixture(scope='module')
+def unpack_message(event_loop):
+    @asyncio.coroutine
+    def _unpack_message(client, key=None, timeout=0.1):
         data = yield from asyncio.wait_for(client.recv(), timeout, loop=event_loop)
         receiver = data[0]
         message = umsgpack.unpackb(data[1:])
         return receiver, message
-    return _get_unencrypted_packet
+    return _unpack_message
+
+
+@pytest.fixture(scope='module')
+def pack_message(event_loop):
+    @asyncio.coroutine
+    def _pack_message(client, receiver, message, key=None, timeout=0.1):
+        receiver = struct.pack('!B', receiver)
+        data = b''.join((receiver, umsgpack.packb(message)))
+        yield from asyncio.wait_for(client.send(data), timeout, loop=event_loop)
+    return _pack_message
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -2,8 +2,6 @@
 The tests provided in this module make sure that the server is
 compliant to the SaltyRTC protocol.
 """
-import asyncio
-
 import pytest
 
 import saltyrtc
@@ -11,36 +9,60 @@ import saltyrtc
 
 class TestProtocol:
     @pytest.mark.asyncio
-    def test_no_subprotocols(self, ws_client_factory):
+    def test_no_subprotocols(self, sleep, ws_client_factory):
         """
         The server must drop the client after the connection has been
         established with a close code of *1002*.
         """
         client = yield from ws_client_factory(subprotocols=None)
-        yield from asyncio.sleep(0.05)
+        yield from sleep(0.05)
         assert not client.open
         assert client.close_code == saltyrtc.CloseCode.sub_protocol_error
 
     @pytest.mark.asyncio
-    def test_invalid_subprotocols(self, ws_client_factory):
+    def test_invalid_subprotocols(self, sleep, ws_client_factory):
         """
         The server must drop the client after the connection has been
         established with a close code of *1002*.
         """
         client = yield from ws_client_factory(subprotocols=['kittie-protocol-3000'])
-        yield from asyncio.sleep(0.05)
+        yield from sleep(0.05)
         assert not client.open
         assert client.close_code == saltyrtc.CloseCode.sub_protocol_error
 
     @pytest.mark.asyncio
-    def test_server_hello(self, ws_client_factory, get_unencrypted_packet):
+    def test_invalid_path(self, url, sleep, ws_client_factory):
+        """
+        The server must drop the client after the connection has been
+        established with a close code of *3001*.
+        """
+        client = yield from ws_client_factory(path='{}/{}'.format(url, 'rawr!!!'))
+        yield from sleep(0.05)
+        assert not client.open
+        assert client.close_code == saltyrtc.CloseCode.protocol_error
+
+    @pytest.mark.asyncio
+    def test_server_hello(self, client_factory):
         """
         The server must send a valid `server-hello` on connection.
         """
-        client = yield from ws_client_factory()
-        receiver, message = yield from get_unencrypted_packet(client)
+        client = yield from client_factory()
+        receiver, message = yield from client.recv()
         assert receiver == 0x00
         assert message['type'] == 'server-hello'
         assert len(message['key']) == 32
         assert len(message['my-cookie']) == 16
-        yield from client.close()
+        yield from client.ws_client.close()
+
+    @pytest.mark.asyncio
+    def test_invalid_client_hello(self, sleep, client_factory):
+        """
+        The server must close the connection when an invalid packet has
+        been sent during the handshake with a close code of *3001*.
+        """
+        client = yield from client_factory()
+        yield from client.recv()
+        yield from client.send(0x00, {'type': 'meow-hello'})
+        yield from sleep(0.05)
+        assert not client.ws_client.open
+        assert client.ws_client.close_code == saltyrtc.CloseCode.protocol_error
