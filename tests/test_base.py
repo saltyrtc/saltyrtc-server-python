c77802b4212f3ce8463fdeb248520790925cb3d2
Lennart Grahl <lennart.grahl@gmail.com>
2016-05-14 21:39:38 +0200

    Fix ping-pong test

--- tests/test_base.py
+++ tests/test_base.py
@@ -11,17 +11,20 @@ class TestPrerequisities:
     def test_server_handshake(self, ws_client_factory):
         """
         Make sure the server is reachable and we can do a simple
-        WebSocket handshake using the correct subprotocol.
+        WebSocket handshake using the correct sub-protocol.
         """
         client = yield from ws_client_factory()
         assert isinstance(client, websockets.client.WebSocketClientProtocol)
+        assert client.subprotocol in pytest.saltyrtc.subprotocols
         yield from client.close()
 
     @pytest.mark.asyncio
     def test_server_ping(self, ws_client_factory):
         """
-        Make sure that we can ping the server.
+        Make sure that we can *ping* the server and the server sends a
+        *pong* response.
         """
         client = yield from ws_client_factory()
-        yield from client.ping()
+        pong = yield from client.ping()
+        yield from pong
         yield from client.close()
