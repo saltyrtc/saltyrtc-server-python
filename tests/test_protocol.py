1cda977eff5a2edaa0de82882ef2e7d1611329b7
Lennart Grahl <lennart.grahl@gmail.com>
2016-05-14 21:54:25 +0200

    Add tests for invalid and no provided sub-protocols

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -2,10 +2,36 @@
 The tests provided in this module make sure that the server is
 compliant to the SaltyRTC protocol.
 """
+import asyncio
+
 import pytest
+
+import saltyrtc
 
 
 class TestProtocol:
+    @pytest.mark.asyncio
+    def test_no_subprotocols(self, ws_client_factory):
+        """
+        The server must drop the client after the connection has been
+        established with a close code of *1002*.
+        """
+        client = yield from ws_client_factory(subprotocols=None)
+        yield from asyncio.sleep(0.05)
+        assert not client.open
+        assert client.close_code == saltyrtc.CloseCode.sub_protocol_error
+
+    @pytest.mark.asyncio
+    def test_invalid_subprotocols(self, ws_client_factory):
+        """
+        The server must drop the client after the connection has been
+        established with a close code of *1002*.
+        """
+        client = yield from ws_client_factory(subprotocols=['kittie-protocol-3000'])
+        yield from asyncio.sleep(0.05)
+        assert not client.open
+        assert client.close_code == saltyrtc.CloseCode.sub_protocol_error
+
     @pytest.mark.asyncio
     def test_server_hello(self, ws_client_factory, get_unencrypted_packet):
         """
