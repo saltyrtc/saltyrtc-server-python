29dc4f8a59f154fe107ddee65623dbcb62a5d7d2
Lennart Grahl <lennart.grahl@gmail.com>
2016-09-23 18:44:24 +0200

    Use matrix

--- .travis-install-uvloop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if python -c "import sys; import os; sys.exit(sys.version_info >= (3,5) and os.environ.get('EVENT_LOOP') == 'uvloop')"; then
-    pip install uvloop;
-fi
-
-exit 0;
--- .travis-run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if python -c "import sys; import os; sys.exit(sys.version_info >= (3,5) or os.environ.get('EVENT_LOOP') == 'asyncio')"; then
-    py.test --cov-config .coveragerc --cov=saltyrtc --loop=$EVENT_LOOP;
-fi
-
-exit 0;
