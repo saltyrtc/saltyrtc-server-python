0fdafbe724d71f81f4c18d3f473f30ed65da52b0
Lennart Grahl <lennart.grahl@gmail.com>
2016-09-29 14:33:32 +0200

    Improve setup.py (automatically install asyncio if required, add extras 'tests', 'logging' and 'uvloop') Try adding support for Python 3.3 Update description and URL in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import ast
 import os
+import sys
 
 from setuptools import setup, find_packages
 
@@ -14,8 +15,21 @@ def get_version():
 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 # Import long description
 long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# Check python version
+py_version = sys.version_info[:2]
+if py_version < (3, 3):
+    raise Exception("SaltyRTC requires Python >= 3.3.")
+
+# Test requirements
+tests_require = [
+    'pytest>=2.8.7',
+    'pytest-asyncio>=0.3.0',
+    'logbook>=1.0.0'
+],
 
 setup(
     name='saltyrtc',
@@ -27,32 +41,36 @@ setup(
         'click>=6.3',
         'websockets>=3.0',
         'u-msgpack-python>=2.1',
-        'asyncio>=3.4.3',
     ],
-    tests_require=[
-        'pytest>=2.8.7',
-        'pytest-asyncio>=0.3.0',
-    ],
+    tests_require=tests_require,
+    extras_require={
+        ':python_version<="3.4"': ['asyncio>=3.4.3'],
+        'tests': tests_require,
+        'logging': ['logbook>=1.0.0'],
+        'uvloop': ['uvloop>=0.5.3'],
+    },
     include_package_data=True,
-
 
     # PyPI metadata
     author='Lennart Grahl',
     author_email='lennart.grahl@gmail.com',
-    description='A SaltyRTC compliant signalling client and server.',
+    description='A SaltyRTC compliant signalling server.',
     long_description=long_description,
     license='MIT License',
     keywords='webrtc ortc signalling signaling websocket websockets nacl',
-    url='https://www.lgrahl.de/',
+    url='https://github.com/saltyrtc/saltyrtc-server-python',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Communications',
         'Topic :: Internet',
         'Topic :: Security',
