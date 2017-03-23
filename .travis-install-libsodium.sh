6d0e17a4a1011f695c1c204dc52ac16b71cb0c5b
Lennart Grahl <lennart.grahl@gmail.com>
2017-03-23 23:44:27 +0100

    Fix don't clone libsodium into the SaltyRTC directory

--- .travis-install-libsodium.sh
+++ .travis-install-libsodium.sh
@@ -5,10 +5,10 @@ if [ -d "${HOME}/libsodium/lib" ]; then
     exit 0;
 fi
 
-git clone --depth 1 -b stable https://github.com/jedisct1/libsodium.git
-cd libsodium
+cd ${HOME}
+git clone --depth 1 -b stable https://github.com/jedisct1/libsodium.git libsodium-git
+cd libsodium-git
 ./autogen.sh
 ./configure --prefix=${HOME}/libsodium && make && make install
-cd ..
 
 exit 0;
