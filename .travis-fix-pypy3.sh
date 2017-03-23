7b817c11b2517f4a922106f3048e284d5a16a321
Lennart Grahl <lennart.grahl@gmail.com>
2017-03-23 21:45:43 +0100

    Fix pypy3 script

--- .travis-fix-pypy3.sh
+++ .travis-fix-pypy3.sh
@@ -5,10 +5,11 @@ PY_VERSION="3.5"
 PYPY_NAME="pypy${PY_VERSION}-5.7-beta-linux_x86_64-portable"
 
 echo "Unpacking PyPy ${PY_VERSION}"
-wget -O ${HOME}/pypy.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/${PYPY_NAME}.tar.bz2
-tar -jxf ${HOME}/pypy.tar.bz2
+wget https://bitbucket.org/squeaky/portable-pypy/downloads/${PYPY_NAME}.tar.bz2 -O ${HOME}/pypy.tar.bz2
+tar -jxf ${HOME}/pypy.tar.bz2 -C ${HOME}
+mv ${HOME}/${PYPY_NAME} ${HOME}/pypy
 
 echo 'Creating virtual environment'
-${HOME}/pypy/bin/pypy${PY_VERSION} -m venv ${HOME}/pypy
+${HOME}/pypy/bin/pypy${PY_VERSION} -m venv ${HOME}/venv
 
 exit 0;
