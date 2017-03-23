04540e0c70ae3b3446c2c7a1e613d96b1859acda
Lennart Grahl <lennart.grahl@gmail.com>
2017-03-23 21:12:54 +0100

    Simplify pypy3 setup script by using venv

--- .travis-fix-pypy3.sh
+++ .travis-fix-pypy3.sh
@@ -4,20 +4,11 @@ set -ev
 PY_VERSION="3.5"
 PYPY_NAME="pypy${PY_VERSION}-5.7-beta-linux_x86_64-portable"
 
+echo "Unpacking PyPy ${PY_VERSION}"
 wget https://bitbucket.org/squeaky/portable-pypy/downloads/${PYPY_NAME}.tar.bz2
 tar -jxf ${PYPY_NAME}.tar.bz2
-mv ${PYPY_NAME} ${HOME}/pypy
-echo 'Setting up aliases...'
-export PATH=${HOME}/pypy/bin:${PATH}
-ln -s ${HOME}/pypy/bin/pypy${PY_VERSION} ${HOME}/pypy/bin/python
-which python
-python --version
 
-echo 'Setting up pip...'
-python -m ensurepip
-ln -s ${HOME}/pypy/bin/pip{PY_VERSION} ${HOME}/pypy/bin/pip
-ls -l ${HOME}/pypy/bin
-which pip
-pip --version
+echo 'Creating virtual environment'
+${PYPY_NAME}/bin/pypy${PY_VERSION} -m venv ${HOME}/pypy
 
 exit 0;
