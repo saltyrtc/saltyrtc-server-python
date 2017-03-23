906051842632321a0003a7c7be582e20b0f7db8b
Lennart Grahl <lennart.grahl@gmail.com>
2017-03-23 21:40:59 +0100

    Don't download pypy into the saltyrtc directory

--- .travis-fix-pypy3.sh
+++ .travis-fix-pypy3.sh
@@ -5,10 +5,10 @@ PY_VERSION="3.5"
 PYPY_NAME="pypy${PY_VERSION}-5.7-beta-linux_x86_64-portable"
 
 echo "Unpacking PyPy ${PY_VERSION}"
-wget https://bitbucket.org/squeaky/portable-pypy/downloads/${PYPY_NAME}.tar.bz2
-tar -jxf ${PYPY_NAME}.tar.bz2
+wget -O ${HOME}/pypy.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/${PYPY_NAME}.tar.bz2
+tar -jxf ${HOME}/pypy.tar.bz2
 
 echo 'Creating virtual environment'
-${PYPY_NAME}/bin/pypy${PY_VERSION} -m venv ${HOME}/pypy
+${HOME}/pypy/bin/pypy${PY_VERSION} -m venv ${HOME}/pypy
 
 exit 0;
