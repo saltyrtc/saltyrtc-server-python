d798f2e07b5a9143a4a08cb81c1d92ca1165a5fa
Lennart Grahl <lennart.grahl@gmail.com>
2017-07-25 19:25:19 +0200

    Try to decrypt 'client-auth' first, then fall back to 'client-hello'

--- saltyrtc/server/message.py
+++ saltyrtc/server/message.py
@@ -225,19 +225,19 @@ class AbstractBaseMessage(AbstractMessage, metaclass=abc.ABCMeta):
             if not client.authenticated and client.type is None:
                 payload = None
 
-                # Try client-hello (unencrypted)
+                # Try client-auth (encrypted)
                 try:
-                    payload = cls._unpack_payload(data)
+                    payload = cls._unpack_payload(
+                        cls._decrypt_payload(client, nonce, data))
                 except MessageError:
                     pass
 
-                # Try client-auth (encrypted)
+                # Try client-hello (unencrypted)
                 if payload is None:
                     try:
-                        payload = cls._unpack_payload(
-                            cls._decrypt_payload(client, nonce, data))
+                        payload = cls._unpack_payload(data)
                     except MessageError:
-                        pass
+                        payload = None
 
                 # Still no payload?
                 if payload is None:
